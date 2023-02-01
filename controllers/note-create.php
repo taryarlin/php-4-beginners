3596a7c5c4f7088af889f722bdac7c746b12241f
Taryar Linn Phyar <35889976+taryarlin@users.noreply.github.com>
2023-02-01 21:45:02 +0630

    Changed user_id as random number

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($errors)) {
         $db->query("INSERT INTO notes(body, user_id) VALUES (:body, :user_id)", [
             'body' => $_POST['body'],
-            'user_id' => 1
+            'user_id' => rand(1, 5)
         ]);
 
         $_POST['body'] = '';
